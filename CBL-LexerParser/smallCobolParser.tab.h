211fbd9f8cf3ae2f8db99cd912699a284c0c7cf2
hunteregeland <59612067+hunteregeland@users.noreply.github.com>
2022-04-13 14:17:58 -0700

    datadiv

--- CBL-LexerParser/smallCobolParser.tab.h
+++ CBL-LexerParser/smallCobolParser.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
-   Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,88 +33,125 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
-
-#ifndef YY_YY_SMALLCOBOLPARSER_TAB_H_INCLUDED
-# define YY_YY_SMALLCOBOLPARSER_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 1
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    NUMBER = 258,
-    DIGIT = 259,
-    ID = 260,
-    STRING = 261,
-    IDENTIFICATION = 262,
-    ENVIRIONMENT = 263,
-    PROCEDURE = 264,
-    DIVISION = 265,
-    PROGRAMID = 266,
-    DISPLAY = 267,
-    ACCEPT = 268,
-    STOP = 269,
-    RUN = 270,
-    IF = 271,
-    THEN = 272,
-    ENDIF = 273,
-    PERFORM = 274,
-    UNTIL = 275,
-    COUNT = 276,
-    TIMES = 277,
-    PICTURE = 278,
-    PIC = 279,
-    IS = 280,
-    TO = 281,
-    PERIOD = 282,
-    COMMA = 283,
-    GT_OP = 284,
-    LT_OP = 285,
-    EQ_OP = 286,
-    DOUBLE_EQ_OP = 287,
-    GT_EQ_OP = 288,
-    LT_EQ_OP = 289,
-    OPEN_PARENTHESES = 290,
-    CLOSE_PARENTHESES = 291,
-    ADD = 292,
-    NINE = 293,
-    LETTERV = 294,
-    LETTERS = 295,
-    LETTERX = 296
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NUMBER = 258,
+     DIGIT = 259,
+     ID = 260,
+     STRING = 261,
+     IDENTIFICATION = 262,
+     ENVIRIONMENT = 263,
+     DATA = 264,
+     FILE = 265,
+     WORKINGSTORAGE = 266,
+     LINKAGE = 267,
+     SCREEN = 268,
+     SECTION = 269,
+     PROCEDURE = 270,
+     DIVISION = 271,
+     PROGRAMID = 272,
+     DISPLAY = 273,
+     ACCEPT = 274,
+     STOP = 275,
+     RUN = 276,
+     IF = 277,
+     THEN = 278,
+     ENDIF = 279,
+     PERFORM = 280,
+     UNTIL = 281,
+     COUNT = 282,
+     TIMES = 283,
+     PICTURE = 284,
+     PIC = 285,
+     IS = 286,
+     TO = 287,
+     PERIOD = 288,
+     COMMA = 289,
+     GT_OP = 290,
+     LT_OP = 291,
+     EQ_OP = 292,
+     DOUBLE_EQ_OP = 293,
+     GT_EQ_OP = 294,
+     LT_EQ_OP = 295,
+     OPEN_PARENTHESES = 296,
+     CLOSE_PARENTHESES = 297,
+     ADD = 298,
+     NINE = 299,
+     LETTERV = 300,
+     LETTERS = 301,
+     LETTERX = 302
+   };
 #endif
+/* Tokens.  */
+#define NUMBER 258
+#define DIGIT 259
+#define ID 260
+#define STRING 261
+#define IDENTIFICATION 262
+#define ENVIRIONMENT 263
+#define DATA 264
+#define FILE 265
+#define WORKINGSTORAGE 266
+#define LINKAGE 267
+#define SCREEN 268
+#define SECTION 269
+#define PROCEDURE 270
+#define DIVISION 271
+#define PROGRAMID 272
+#define DISPLAY 273
+#define ACCEPT 274
+#define STOP 275
+#define RUN 276
+#define IF 277
+#define THEN 278
+#define ENDIF 279
+#define PERFORM 280
+#define UNTIL 281
+#define COUNT 282
+#define TIMES 283
+#define PICTURE 284
+#define PIC 285
+#define IS 286
+#define TO 287
+#define PERIOD 288
+#define COMMA 289
+#define GT_OP 290
+#define LT_OP 291
+#define EQ_OP 292
+#define DOUBLE_EQ_OP 293
+#define GT_EQ_OP 294
+#define LT_EQ_OP 295
+#define OPEN_PARENTHESES 296
+#define CLOSE_PARENTHESES 297
+#define ADD 298
+#define NINE 299
+#define LETTERV 300
+#define LETTERS 301
+#define LETTERX 302
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-union YYSTYPE
-{
+typedef union YYSTYPE
 #line 21 "smallCobolParser.y"
-
+{
 	int number;
 	char character;
 	char* string;
 	struct AST* ast;
-
-#line 106 "smallCobolParser.tab.h"
-
-};
-typedef union YYSTYPE YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+}
+/* Line 1529 of yacc.c.  */
+#line 150 "smallCobolParser.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_SMALLCOBOLPARSER_TAB_H_INCLUDED  */
